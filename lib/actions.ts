43407da611ca8704c2e4cc81a95a4350e4e7641e
Erenthos <erenthos@gmail.com>
2026-01-17 10:55:44 +0530

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,8 +1,8 @@
 export function calculateVendorClass(financialStrength: number) {
-  if (financialStrength > 200) return "A";
-  if (financialStrength > 100) return "B";
-  if (financialStrength > 50) return "C";
-  if (financialStrength > 10) return "D";
+  if (financialStrength >= 200) return "A";
+  if (financialStrength >= 100) return "B";
+  if (financialStrength >= 50) return "C";
+  if (financialStrength >= 10) return "D";
   return "E";
 }
 
