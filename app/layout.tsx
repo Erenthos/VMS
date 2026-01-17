8cf148844cda912c7f8cd13e5eee45d451618ba0
Erenthos <erenthos@gmail.com>
2026-01-17 10:42:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,22 @@
+import "@/styles/globals.css";
+import { ReactNode } from "react";
+
+export const metadata = {
+  title: "Vendor Management System",
+  description: "Procurement Vendor Master Database"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
 
