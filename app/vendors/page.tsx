c54371cb489bbaaca9924df679da813757698a63
Erenthos <erenthos@gmail.com>
2026-01-17 10:25:09 +0530

    Update page.tsx

--- app/vendors/page.tsx
+++ app/vendors/page.tsx
@@ -1,1 +1,11 @@
+import VendorTable from "@/components/VendorTable";
+
+export default function VendorsPage() {
+  return (
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-semibold">Vendor Master</h1>
+      <VendorTable />
+    </div>
+  );
+}
 
